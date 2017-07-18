b89d6e2c4844ef6c568935b98f2da994c2aaf217
Ludovic Claude <ludovic.claude54@gmail.com>
2017-07-18 16:51:40 +0200

    CircleCI: use version 2.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [ $NO_SUDO ]; then
+if [[ $NO_SUDO || -n "$CIRCLECI" ]]; then
   CAPTAIN="captain"
 elif groups $USER | grep &>/dev/null '\bdocker\b'; then
   CAPTAIN="captain"
