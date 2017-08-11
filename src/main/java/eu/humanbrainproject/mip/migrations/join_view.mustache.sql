e0e44983503bad8a4e0dd831c031abecb21067eb
Ludovic Claude <ludovic.claude@laposte.net>
2017-08-12 01:25:59 +0200

    Use Left outer join for joins

--- src/main/java/eu/humanbrainproject/mip/migrations/join_view.mustache.sql
+++ src/main/java/eu/humanbrainproject/mip/migrations/join_view.mustache.sql
@@ -1,3 +1,3 @@
 
 CREATE OR REPLACE VIEW {{ view.name }} ({{ view.columns }})
-  AS SELECT {{ table1.qualifiedColumns }},{{ table2.qualifiedColumnsNoId }} FROM {{ table1.name }} INNER JOIN {{ table2.name }} ON ({{ table1.qualifiedId }} = {{ table2.qualifiedId }});
+  AS SELECT {{ table1.qualifiedColumns }},{{ table2.qualifiedColumnsNoId }} FROM {{ table1.name }} LEFT OUTER JOIN {{ table2.name }} ON ({{ table1.qualifiedId }} = {{ table2.qualifiedId }});
