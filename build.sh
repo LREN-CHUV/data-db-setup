a21aa07b51c0b6a4436caf003dff2bd57bd026c0
Ludovic Claude <ludovic.claude54@gmail.com>
2018-09-19 09:39:51 +0200

    Autofix: CaptainBuildScript
    [atomist:generated] [atomist:autofix=CaptainBuildScript]

--- build.sh
+++ build.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env bash
-set -o pipefail  # trace ERR through pipes
-set -o errtrace  # trace ERR through 'time command' and other functions
-set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
-
-get_script_dir () {
-     SOURCE="${BASH_SOURCE[0]}"
-
-     while [ -h "$SOURCE" ]; do
-          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-          SOURCE="$( readlink "$SOURCE" )"
-          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-     done
-     cd -P "$( dirname "$SOURCE" )"
-     pwd
-}
-
-cd "$(get_script_dir)"
+set -e
 
 if [[ $NO_SUDO || -n "$CIRCLECI" ]]; then
   CAPTAIN="captain"
@@ -28,4 +12,4 @@ fi
 BUILD_DATE=$(date -Iseconds) \
   VCS_REF=$(git describe --tags --dirty) \
   VERSION=$(git describe --tags --dirty) \
-  $CAPTAIN build
+  $CAPTAIN build  "$@"
