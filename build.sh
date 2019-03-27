bc4729a718fecad1995da4efa084601f68e9a1cb
Ludovic Claude <ludovic.claude54@gmail.com>
2019-03-27 11:02:06 +0100

    Do not use captain anymore for the build

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+
 set -o pipefail  # trace ERR through pipes
 set -o errtrace  # trace ERR through 'time command' and other functions
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
 get_script_dir () {
-  SOURCE="${BASH_SOURCE[0]}"
+     SOURCE="${BASH_SOURCE[0]}"
+
      while [ -h "$SOURCE" ]; do
           DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
           SOURCE="$( readlink "$SOURCE" )"
@@ -16,15 +18,21 @@ get_script_dir () {
 
 cd "$(get_script_dir)"
 
-if [[ $NO_SUDO || -n "$CIRCLECI" ]]; then
-  CAPTAIN="captain"
-elif groups $USER | grep &>/dev/null '\bdocker\b'; then
-  CAPTAIN="captain"
+if [[ "$NO_SUDO" || -n "$CIRCLECI" ]]; then
+  DOCKER="docker"
+elif groups "$USER" | grep &>/dev/null '\bdocker\b'; then
+  DOCKER="docker"
 else
-  CAPTAIN="sudo captain"
+  DOCKER="sudo docker"
 fi
 
-BUILD_DATE=$(date -Iseconds) \
-  VCS_REF=$(git describe --tags --dirty) \
-  VERSION=$(git describe --tags --dirty) \
-  $CAPTAIN build "$@"
+source ./.dockerimage
+VCS_REF=$(git describe --tags --dirty)
+VERSION=$(git describe --tags --dirty)
+
+docker build --build-arg BUILD_DATE=$(date -Iseconds) \
+    --build-arg VCS_REF=$VCS_REF \
+    --build-arg VERSION=$VERSION \
+    --tag "$IMAGE:latest" \
+    --tag "$IMAGE:$VERSION" \
+    .
